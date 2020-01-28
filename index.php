a5c5cb60579fdb182c0cf9a847859d95266c6832
md-riaz <riazmd582@gmail.com>
2020-01-28 20:12:00 +0600

    favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="https://img.icons8.com/cute-clipart/64/000000/external-link.png" type="image/x-icon">
   <title>Link Copier</title>
   <style>
     body {
