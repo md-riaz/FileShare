7bf93c02908844489bd0df316c9c24b54ff87129
mdriaz <riazmd582@gmail.com>
2021-03-10 09:42:42 +0600

    added unique id to result output.

--- upload/PHPUploader.php
+++ upload/PHPUploader.php
@@ -5,21 +5,23 @@ class PHPUploader {
 	private $destinationPath;
 	private $errorMessage;
 	private $extensions;
+	private $mimetypes;
 	private $allowAll;
 	private $maxSize;
 	private $uploadName;
 
 	public function __construct()
 	{
+		$this->allowAll = false;
 		$this->maxSize = (min((int) ini_get('post_max_size'), (int) ini_get('upload_max_filesize')) * 1024 * 1024);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      // server max upload size in bits
-		$this->extensions = ['aac' => 'audio/aac', 'ai' => 'application/postscript', 'avi' => 'video/x-msvideo', 'bmp' => 'image/bmp', 'class' => 'application/octet-stream', 'css' => 'text/css', 'csv' => 'text/csv', 'dll' => 'application/octet-stream', 'dmg' => 'application/octet-stream', 'doc' => 'application/msword', 'dvi' => 'application/x-dvi', 'eps' => 'application/postscript', 'exe' => 'application/octet-stream', 'gif' => 'image/gif', 'htm' => 'text/html', 'html' => 'text/html', 'ico' => 'image/x-icon', 'jpe' => 'image/jpeg', 'jpeg' => 'image/jpeg', 'jpg' => 'image/jpeg', 'js' => 'application/x-javascript', 'json' => 'application/json', 'mov' => 'video/quicktime', 'movie' => 'video/x-sgi-movie', 'mp2' => 'audio/mpeg', 'mp3' => 'audio/mpeg', 'mpe' => 'video/mpeg', 'mpeg' => 'video/mpeg', 'mpg' => 'video/mpeg', 'mpga' => 'audio/mpeg', 'ogg' => 'application/ogg', 'pdf' => 'application/pdf', 'png' => 'image/png', 'php' => 'application/x-httpd-php', 'ppt' => 'application/vnd.ms-powerpoint', 'ps' => 'application/postscript', 'rar' => 'application/vnd.rar', 'rgb' => 'image/x-rgb', 'rss' => 'application/rss+xml', 'svg' => 'image/svg+xml', 'svgz' => 'image/svg+xml', 'swf' => 'application/x-shockwave-flash', 'tif' => 'image/tiff', 'tiff' => 'image/tiff', 'txt' => 'text/plain', 'vcd' => 'application/x-cdlink', 'wav' => 'audio/x-wav', 'wmv' => 'video/x-ms-wmv', 'wbmp' => 'image/vnd.wap.wbmp', 'wbxml' => 'application/vnd.wap.wbxml', 'xhtml' => 'application/xhtml+xml', 'xls' => 'application/vnd.ms-excel', 'xml' => 'application/xml', 'xsl' => 'application/xml', 'xul' => 'application/vnd.mozilla.xul+xml', 'zip' => 'application/zip', '7z' => 'application/x-7z-compressed', 'sql' => 'text/x-Algol68']; // some default extensions
+		$this->mimetypes = ['aac' => 'audio/aac', 'ai' => 'application/postscript', 'avi' => 'video/x-msvideo', 'bmp' => 'image/bmp', 'class' => 'application/octet-stream', 'css' => 'text/css', 'csv' => 'text/csv', 'dll' => 'application/octet-stream', 'dmg' => 'application/octet-stream', 'doc' => 'application/msword', 'dvi' => 'application/x-dvi', 'eps' => 'application/postscript', 'exe' => 'application/octet-stream', 'gif' => 'image/gif', 'htm' => 'text/html', 'html' => 'text/html', 'ico' => 'image/x-icon', 'jpe' => 'image/jpeg', 'jpeg' => 'image/jpeg', 'jpg' => 'image/jpeg', 'js' => 'application/x-javascript', 'json' => 'application/json', 'mov' => 'video/quicktime', 'movie' => 'video/x-sgi-movie', 'mp2' => 'audio/mpeg', 'mp3' => 'audio/mpeg', 'mpe' => 'video/mpeg', 'mpeg' => 'video/mpeg', 'mpg' => 'video/mpeg', 'mpga' => 'audio/mpeg', 'ogg' => 'application/ogg', 'pdf' => 'application/pdf', 'png' => 'image/png', 'php' => 'application/x-httpd-php', 'ppt' => 'application/vnd.ms-powerpoint', 'ps' => 'application/postscript', 'rar' => 'application/vnd.rar', 'rgb' => 'image/x-rgb', 'rss' => 'application/rss+xml', 'svg' => 'image/svg+xml', 'svgz' => 'image/svg+xml', 'swf' => 'application/x-shockwave-flash', 'tif' => 'image/tiff', 'tiff' => 'image/tiff', 'txt' => 'text/plain', 'vcd' => 'application/x-cdlink', 'wav' => 'audio/x-wav', 'wmv' => 'video/x-ms-wmv', 'wbmp' => 'image/vnd.wap.wbmp', 'wbxml' => 'application/vnd.wap.wbxml', 'xhtml' => 'application/xhtml+xml', 'xls' => 'application/vnd.ms-excel', 'xml' => 'application/xml', 'xsl' => 'application/xml', 'xul' => 'application/vnd.mozilla.xul+xml', 'zip' => 'application/zip', '7z' => 'application/x-7z-compressed', 'sql' => 'text/x-Algol68']; // some default mime types
 	}
 
 
 	function setDir($path)
 	{
 		$this->destinationPath = $path;
-		$this->allowAll = false;
+
 	}
 
 	function allowAllFormats()
@@ -57,14 +59,19 @@ class PHPUploader {
 		$name = $_FILES[$fileBrowse]["name"];
 		$tmp_name = $_FILES[$fileBrowse]["tmp_name"];
 		$ext = $this->getExtension($name);
+
 		if (!is_dir($this->destinationPath)) {
 			$this->setMessage("Destination folder is not a directory ");
+
 		} else if (!is_writable($this->destinationPath)) {
 			$this->setMessage("Destination is not writable !");
+
 		} else if (empty($name)) {
 			$this->setMessage("File not selected ");
+
 		} else if ($size > $this->maxSize) {
 			$this->setMessage("Too large file ! max size is {$this->maxSize}");
+
 		} else if ($this->allowAll || (!$this->allowAll && $this->checkMIMEType($tmp_name))) {
 
 			if ($this->existsFile($name)) {
@@ -72,12 +79,15 @@ class PHPUploader {
 			} else {
 				$this->uploadName = $name;
 			}
+
 			if (move_uploaded_file($tmp_name, $this->destinationPath . $this->uploadName)) {
 				$result = true;
 			} else {
 				$this->setMessage("Upload failed , try later !");
 			}
+
 		} else {
+
 			$this->setMessage("Invalid file format !");
 		}
 
@@ -94,11 +104,11 @@ class PHPUploader {
 		$this->errorMessage = $message;
 	}
 
-	public function checkMIMEType($tmp_name)
+	function checkMIMEType($tmp_name)
 	{
 		// Check MIME Type by yourself.
 		$finfo = new finfo(FILEINFO_MIME_TYPE);
-		if (!in_array($finfo->file($tmp_name), $this->extensions, true)) {
+		if (!in_array($finfo->file($tmp_name), $this->mimetypes, true)) {
 			return false;
 		}
 
--- upload/upload.php
+++ upload/upload.php
@@ -1,19 +1,29 @@
 <?php
 require_once('PHPUploader.php');
-
+$unique_id = $_POST['unique_id'];
 $uploader = new PHPUploader();
 $uploader->setDir('shared_files/');
 
-$uploader->setMaxSize(8);       //set max file size to be allowed in MB//
-$result = $uploader->uploadFile('file_input');
-if ($result) {      //file_input is the filebrowse element name //
-	echo json_encode(['status' => 200, 'message' => 'File is uploaded successfully.']);
+$uploader->setMaxSize(8); //set max file size to be allowed in MB
+
+$result = $uploader->uploadFile('file_input'); //file_input is the filebrowse element name
+
+if ($result) {
+
+	echo json_encode([
+		'id' => $unique_id,
+		'status'  => 200,
+		'message' => 'File is uploaded successfully.'
+	]);
 	exit;
 }
 
-//upload failed
-//get upload error message
-echo json_encode(['status' => 400, 'message' => $uploader->getMessage()]);
+//upload failed || get upload error message
+echo json_encode([
+	'id' => $unique_id,
+	'status'  => 400,
+	'message' => $uploader->getMessage()
+]);
 exit;
 
 ?>
